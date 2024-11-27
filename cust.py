3569d4ffe9887cfd3253a782e0bed897d9ec41ca
VladSkopenko <skopirka2k17@gmail.com>
2024-11-27 18:42:49 +0200

    add my

---
+++ cust.py
@@ -0,0 +1,17 @@
+# Моє особисте  рішення після того як я розібрався
+def fi(n):
+    result = []
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
+    x = 0
+    y = 1
+    result.append(x)
+    for i in range(n):
+        x, y = y, x + y
+        result.append(x)
+    return result
+
+if __name__ == '__main__':
+    print(fi(10))
