e301750fb0250d3088e5a2dd591f5fb660f0557d
VladSkopenko <skopirka2k17@gmail.com>
2024-11-29 19:50:58 +0200

    update with recurs

--- me.py
+++ me.py
@@ -14,5 +14,19 @@ def fi(n):
     return result
 
 
+def fi_recursive(n):
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
+    if n == 2:
+        return [0, 1]
+
+    previous = fi_recursive(n - 1)
+    next_value = previous[-1] + previous[-2]
+    return previous + [next_value]
+
+
 if __name__ == '__main__':
     print(fi(10))
+    print(fi_recursive(10))
