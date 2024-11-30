a35dd86ee1fa6f7278d69817945ab3b3308ff36b
VladSkopenko <skopirka2k17@gmail.com>
2024-11-30 18:03:21 +0200

    add solution from linkedin

---
+++ from_linkedin_solve_Serhii_Kodenko.py
@@ -0,0 +1,5 @@
+from itertools import accumulate
+
+n = 10
+start = 1  # 0|1 - з чого починаємо
+print([i[start] for i in accumulate(range(n - 1), lambda s, _: (s[1], s[0] + s[1]), initial=(0, 1))])
